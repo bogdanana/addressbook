93f2dd1a05fbf61badad5ccab1f798293addbee9
bogdanana <bogdana144@gmail.com>
2020-08-13 19:02:54 +0300

    contactCreation

---
+++ src/test/java/adressbook/ContactData.java
@@ -0,0 +1,31 @@
+package adressbook;
+
+public class ContactData {
+    private final String firstname;
+    private final String lastname;
+    private final String email;
+    private final String home;
+
+    public ContactData(String firstname, String lastname, String email, String home) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.home = home;
+    }
+
+    public String getFirstName() {
+        return firstname;
+    }
+
+    public String getLastName() {
+        return lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getHome() {
+        return home;
+    }
+}
